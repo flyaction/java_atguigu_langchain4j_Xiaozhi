7b2d05ea7b8dcd7bb0b472a78d14719cb7932e67
action <xingdong1117@126.com>
2025-11-27 15:44:47 +0800

    feat:构建项目

---
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/XiaozhiApp.java
@@ -0,0 +1,15 @@
+package com.atguigu.java.ai.langchain4j;
+
+/**
+ * @author: action
+ * @create: 2025/11/27 15:41
+ **/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiApp.class, args);
+    }
+}
