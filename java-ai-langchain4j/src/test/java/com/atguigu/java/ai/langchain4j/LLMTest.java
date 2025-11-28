455530a7e6fdf1fa6eda78f26d8cfbf00dff5764
action <xingdong1117@126.com>
2025-11-28 10:57:52 +0800

    feat:使用spring-boot-starter

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
@@ -2,6 +2,7 @@ package com.atguigu.java.ai.langchain4j;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -27,5 +28,19 @@ public class LLMTest {
         //输出结果
         System.out.println(answer);
     }
+
+    /**
+     * 整合SpringBoot
+     */
+    @Autowired
+    private OpenAiChatModel openAiChatModel;
+    @Test
+    public void testSpringBoot() {
+        //向模型提问
+        String answer = openAiChatModel.chat("你好");
+        //输出结果
+        System.out.println(answer);
+    }
+
 
 }
