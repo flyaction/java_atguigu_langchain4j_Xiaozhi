f28ff09471257f70c4d130a5d16d460f42b74209
action <xingdong1117@126.com>
2025-12-01 15:35:09 +0800

    feat:聊天记忆的简单实现

---
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ChatMemoryTest.java
@@ -0,0 +1,52 @@
+package com.atguigu.java.ai.langchain4j;
+
+import com.atguigu.java.ai.langchain4j.assistant.Assistant;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+/**
+ * @author: action
+ * @create: 2025/12/1 15:24
+ **/
+@SpringBootTest
+public class ChatMemoryTest {
+
+    @Autowired
+    private Assistant assistant;
+
+    @Test
+    public void testChatMemory() {
+        String answer1 = assistant.chat("我是欢欢");
+        System.out.println(answer1);
+
+        String answer2 = assistant.chat("我是谁");
+        System.out.println(answer2);
+    }
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+    @Test
+    public void testChatMemory2() {
+        //第一轮对话
+        UserMessage userMessage1 = UserMessage.userMessage("我是环环");
+        ChatResponse chatResponse1 = qwenChatModel.chat(userMessage1);
+        AiMessage aiMessage1 = chatResponse1.aiMessage();
+
+        //输出大语言模型的回复
+        System.out.println(aiMessage1.text());
+        //第二轮对话
+        UserMessage userMessage2 = UserMessage.userMessage("你知道我是谁吗");
+        ChatResponse chatResponse2 = qwenChatModel.chat(Arrays.asList(userMessage1, aiMessage1, userMessage2));
+        AiMessage aiMessage2 = chatResponse2.aiMessage();
+        //输出大语言模型的回复
+        System.out.println(aiMessage2.text());
+    }
+
+}
