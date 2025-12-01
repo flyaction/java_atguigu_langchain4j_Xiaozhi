6f05ff788e0a43c7a02cb5b9a47319df5d0a48e2
action <xingdong1117@126.com>
2025-12-01 15:40:52 +0800

    feat:使用ChatMemory实现聊天记忆

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ChatMemoryTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ChatMemoryTest.java
@@ -4,7 +4,9 @@ import com.atguigu.java.ai.langchain4j.assistant.Assistant;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.service.AiServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,6 +49,24 @@ public class ChatMemoryTest {
         AiMessage aiMessage2 = chatResponse2.aiMessage();
         //输出大语言模型的回复
         System.out.println(aiMessage2.text());
+    }
+
+
+    @Test
+    public void testChatMemory3() {
+        //创建chatMemory
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        //创建AIService
+        Assistant assistant = AiServices
+                .builder(Assistant.class)
+                .chatLanguageModel(qwenChatModel)
+                .chatMemory(chatMemory)
+                .build();
+        //调用service的接口
+        String answer1 = assistant.chat("我是环环");
+        System.out.println(answer1);
+        String answer2 = assistant.chat("我是谁");
+        System.out.println(answer2);
     }
 
 }
