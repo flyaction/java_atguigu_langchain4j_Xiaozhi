09b8cb52bfa44a15aa705ac6146a057f337f3bc8
action <xingdong1117@126.com>
2025-12-01 10:03:34 +0800

    feat:AIServices测试

---
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/Assistant.java
@@ -0,0 +1,14 @@
+package com.atguigu.java.ai.langchain4j.assistant;
+
+import dev.langchain4j.service.spring.AiService;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
+/**
+ * @author: action
+ * @create: 2025/12/1 09:51
+ **/
+@AiService(wiringMode = EXPLICIT, chatModel = "qwenChatModel")
+public interface Assistant {
+    String chat(String userMessage);
+}
