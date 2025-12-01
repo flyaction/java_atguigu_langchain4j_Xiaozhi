242e32b080c3b38d75a16f2cbfd0fa8db774b4a6
action <xingdong1117@126.com>
2025-12-01 15:47:30 +0800

    feat:使用AiService实现聊天记忆

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ChatMemoryTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ChatMemoryTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.java.ai.langchain4j;
 
 import com.atguigu.java.ai.langchain4j.assistant.Assistant;
+import com.atguigu.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
@@ -66,6 +67,16 @@ public class ChatMemoryTest {
         String answer1 = assistant.chat("我是环环");
         System.out.println(answer1);
         String answer2 = assistant.chat("我是谁");
+        System.out.println(answer2);
+    }
+
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+    @Test
+    public void testChatMemory4() {
+        String answer1 = memoryChatAssistant.chat("我是环环");
+        System.out.println(answer1);
+        String answer2 = memoryChatAssistant.chat("我是谁");
         System.out.println(answer2);
     }
 
