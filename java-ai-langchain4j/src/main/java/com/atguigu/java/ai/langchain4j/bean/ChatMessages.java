6670381389627c1f4156e88bbfb497d237059746
action <xingdong1117@126.com>
2025-12-02 13:40:26 +0800

    feat:Persistence实现 by Mongodb

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/bean/ChatMessages.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/bean/ChatMessages.java
@@ -21,7 +21,7 @@ public class ChatMessages {
     //private Long messageId;
     private ObjectId messageId;
 
-
+    private String memoryId;
 
     private String content; //存储当前聊天记录列表的json字符串
 }
--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -1,9 +1,11 @@
 package com.atguigu.java.ai.langchain4j.config;
 
+import com.atguigu.java.ai.langchain4j.store.MongoChatMemoryStore;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +16,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SeparateChatAssistantConfig {
 
+    @Autowired
+    private MongoChatMemoryStore mongoChatMemoryStore;
     @Bean
     public ChatMemoryProvider chatMemoryProvider(){
        return memoryId -> MessageWindowChatMemory.builder()
                .id(memoryId)
                .maxMessages(10)
-               .chatMemoryStore(new InMemoryChatMemoryStore())
+               //.chatMemoryStore(new InMemoryChatMemoryStore())
+               .chatMemoryStore(mongoChatMemoryStore)
                .build();
    }
 }
