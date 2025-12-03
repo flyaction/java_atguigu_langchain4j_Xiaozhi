eff8513865eea7ad714fdc2a1c6adefe4b43c6e3
action <xingdong1117@126.com>
2025-12-03 14:31:34 +0800

    feat:prompt用户提示词

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/PromptTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/PromptTest.java
@@ -1,5 +1,6 @@
 package com.atguigu.java.ai.langchain4j;
 
+import com.atguigu.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import com.atguigu.java.ai.langchain4j.assistant.SeparateChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,20 @@ public class PromptTest {
     public void testSystemMessage() {
         String answer = separateChatAssistant.chat(3,"今天几号");
         System.out.println(answer);
+    }
+
+
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+    @Test
+    public void testUserMessage() {
+        String answer1 = memoryChatAssistant.chat("我是环环");
+        System.out.println(answer1);
+        String answer2 = memoryChatAssistant.chat("我18了");
+        System.out.println(answer2);
+        String answer3 = memoryChatAssistant.chat("你知道我是谁吗");
+        System.out.println(answer3);
+
     }
 
 }
