7d4302214db99bb391e90d31da96ddd3f9071ce1
action <xingdong1117@126.com>
2025-12-04 10:29:21 +0800

    feat:测试数据库增删查

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/mapper/AppointmentMapper.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/mapper/AppointmentMapper.java
@@ -2,6 +2,7 @@ package com.atguigu.java.ai.langchain4j.mapper;
 
 import com.atguigu.java.ai.langchain4j.entity.Appointment;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author action
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2025-12-04 10:07:25
 * @Entity com.atguigu.java.ai.langchain4j.entity.Appointment
 */
+@Mapper
 public interface AppointmentMapper extends BaseMapper<Appointment> {
 
 }
