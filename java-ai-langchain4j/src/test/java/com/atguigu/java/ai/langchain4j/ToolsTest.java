eb8b1f4a95f8d6967cab5233ca275594295f5ce8
action <xingdong1117@126.com>
2025-12-04 09:50:46 +0800

    feat:工具相关注解@ToolMemoryId

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ToolsTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/ToolsTest.java
@@ -15,7 +15,7 @@ public class ToolsTest {
     private SeparateChatAssistant separateChatAssistant;
     @Test
     public void testCalculatorTools() {
-        String answer = separateChatAssistant.chat(10, "1+2等于几，475695037565的平方根是多 少？");
+        String answer = separateChatAssistant.chat(5, "1+2等于几，475695037565的平方根是多 少？");
         //答案：3，689706.4865
         System.out.println(answer);
     }
