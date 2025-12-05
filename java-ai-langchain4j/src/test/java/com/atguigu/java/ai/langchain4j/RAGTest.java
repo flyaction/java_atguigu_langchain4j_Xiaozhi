bbc0a420510609c0591c24be40c1f33dcb6cc8b0
action <xingdong1117@126.com>
2025-12-05 08:56:30 +0800

    feat:文档加载器txt+md文档

---
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/RAGTest.java
@@ -0,0 +1,49 @@
+package com.atguigu.java.ai.langchain4j;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.util.List;
+
+/**
+ * @author: action
+ * @create: 2025/12/5 08:32
+ **/
+@SpringBootTest
+public class RAGTest {
+    @Test
+    public void testReadDocument() {
+
+        String filePath = "/Users/action/code/study_java/java_atguigu_langchain4j_Xiaozhi/knowledge";
+
+        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+        //并使用默认的文档解析器TextDocumentParser对文档进行解析
+        //Document document = FileSystemDocumentLoader.loadDocument("/Users/action/code/study_java/java_atguigu_langchain4j_Xiaozhi/knowledge/测试.txt");
+        //System.out.println(document.text());
+
+        // 加载单个文档
+        //Document document = FileSystemDocumentLoader.loadDocument(filePath+"/file.txt", new TextDocumentParser());
+
+        // 从一个目录中加载所有文档
+        //List<Document> documents = FileSystemDocumentLoader.loadDocuments(filePath, new TextDocumentParser());
+
+        // 从一个目录中加载所有的.txt文档
+        //PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.txt");
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.md");
+        List<Document> documents2 = FileSystemDocumentLoader.loadDocuments(filePath, pathMatcher, new TextDocumentParser());
+        for(Document document : documents2){
+            System.out.println("=====================");
+            System.out.println(document.metadata());
+            System.out.println(document.text());
+        }
+
+        // 从一个目录及其子目录中加载所有文档
+        //List<Document> documents3 = FileSystemDocumentLoader.loadDocumentsRecursively(filePath, new TextDocumentParser());
+
+    }
+}
