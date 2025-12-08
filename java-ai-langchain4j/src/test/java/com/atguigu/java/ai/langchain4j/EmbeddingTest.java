7b98bee4324d0bbfdb7c8a26c959fee18a0ab468
action <xingdong1117@126.com>
2025-12-08 13:41:25 +0800

    feat:上传知识库到Pinecone

--- java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/EmbeddingTest.java
+++ java-ai-langchain4j/src/test/java/com/atguigu/java/ai/langchain4j/EmbeddingTest.java
@@ -1,16 +1,18 @@
 package com.atguigu.java.ai.langchain4j;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.output.Response;
-import dev.langchain4j.store.embedding.EmbeddingMatch;
-import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
-import dev.langchain4j.store.embedding.EmbeddingSearchResult;
-import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author: action
@@ -76,8 +78,24 @@ public class EmbeddingTest {
         System.out.println(embeddingMatch.embedded().text());
     }
 
+    private String filePath = "/Users/action/code/study_java/java_atguigu_langchain4j_Xiaozhi/knowledge";
 
-
+    @Test
+    public void testUploadKnowledgeLibrary() {
+        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+        //并使用默认的文档解析器对文档进行解析
+        Document document1 = FileSystemDocumentLoader.loadDocument(filePath+"/医院信息.md");
+        Document document2 = FileSystemDocumentLoader.loadDocument(filePath+"/科室信息.md");
+        Document document3 = FileSystemDocumentLoader.loadDocument(filePath+"/神经内科.md");
+        List<Document> documents = Arrays.asList(document1, document2, document3);
+        //文本向量化并存入向量数据库：将每个片段进行向量化，得到一个嵌入向量
+        EmbeddingStoreIngestor
+                .builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .build()
+                .ingest(documents);
+    }
 
 
 
