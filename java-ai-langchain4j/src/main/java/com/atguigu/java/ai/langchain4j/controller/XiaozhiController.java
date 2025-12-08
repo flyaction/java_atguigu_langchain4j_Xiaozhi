4c8c3c0a30a8bb08d03f80b6b82cb071a75df620
action <xingdong1117@126.com>
2025-12-08 18:06:48 +0800

    feat:流式输出

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/controller/XiaozhiController.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/controller/XiaozhiController.java
@@ -1,6 +1,7 @@
 package com.atguigu.java.ai.langchain4j.controller;
 
 import com.atguigu.java.ai.langchain4j.assistant.XiaozhiAgent;
+import com.atguigu.java.ai.langchain4j.assistant.XiaozhiStreamAgent;
 import com.atguigu.java.ai.langchain4j.bean.ChatForm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * @author: action
@@ -20,9 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class XiaozhiController {
     @Autowired
     private XiaozhiAgent xiaozhiAgent;
+
+    @Autowired
+    private XiaozhiStreamAgent xiaozhiStreamAgent;
+
     @Operation(summary = "对话")
     @PostMapping("/chat")
     public String chat(@RequestBody ChatForm chatForm) {
         return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+    }
+
+    @Operation(summary = "流式对话")
+    @PostMapping(value = "/stream",produces = "text/stream;charset=utf-8")
+    public Flux<String> stream(@RequestBody ChatForm chatForm) {
+        return xiaozhiStreamAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
